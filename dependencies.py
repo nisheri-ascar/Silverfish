cb74b0bc61dd44dbaec7c60c1efa528818194f3d
nisheri-ascar <kertkert704@gmail.com>
2023-07-19 17:47:23 +0800

    Saner way to deal with pip

--- dependencies.py
+++ dependencies.py
@@ -14,18 +14,14 @@ if sys.version_info < (3, 8):
 
 # Make sure pip is installed
 logging.info("Making sure pip is installed...")
-try: # Debian and Ubuntu require pip to be installed differently because they disable ensurepip
-    import distro
-    linuxDistro = distro.linux_distribution()
-    if linuxDistro[0] == "Debian" or linuxDistro[0] == "Ubuntu":
-        try:
-            subprocess.check_call(['apt', 'install', 'python3-pip', '-y'])
-        except subprocess.CalledProcessError:
-            logging.fatal("You need to be the superuser to install pip and the dependencies")
-            sys.exit(-4)
-except ModuleNotFoundError: # If the system is not Linux, the module "distro" will not load, causing a ModuleNotFoundError exception and the normal "ensurepip" module will work
-    subprocess.check_call([sys.executable, '-m', 'ensurepip']) # The module ensure pip check if pip is installed and installs it if it isn't
-logging.info("Pip is installed")
+# Instead of previous debian specific solution, tell user instead to install it. 
+try:
+    subprocess.check_call([sys.executable, '-m' 'pip' '-V'])
+except subprocess.CalledProcessError:
+    logging.fatal("pip is not installed. You can find resources online on how to install it specifically for your operating system.")
+    sys.exit(-4)
+logging.info("Pip is present")
+
 
 # Make sure that dependencies are installed
 logging.info("Installing dependencies...")
